fd4f54121dd3d6016efc60f20fee0579f6911a79
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-08-11 15:57:10 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.cpp
@@ -0,0 +1,52 @@
+class DynamicArray {
+public:
+
+    DynamicArray(int capacity) {
+        this->capacity = capacity;
+        this->size = 0;
+        arr = new int[capacity];
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if (size == capacity) {
+            resize();
+        }
+        arr[size] = n;
+        size++;
+    }
+
+    int popback() {
+        size--;
+        return arr[size];
+    }
+
+    void resize() {
+        capacity *= 2;
+        int *new_arr = new int[capacity];
+        for (int i = 0; i < size; i++) {
+            new_arr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = new_arr;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+private :
+int size;
+int capacity;
+int *arr;
+};
