d632afb7178de1aaf67c8dd721a66683b87b3ef0
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-08-12 17:04:12 +0300

    Add: dynamicArray - submission-5

---
+++ Data Structures & Algorithms/dynamicArray/submission-5.ts
@@ -0,0 +1,81 @@
+class DynamicArray {
+    capacity: number
+    size: number
+    arr :number []
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity: number) {
+        this.capacity = capacity
+        this.size = 0
+        this.arr = new Array(capacity)
+
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i: number): number {
+        return this.arr[i]
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i: number, n: number): void {
+        this.arr[i] = n
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n: number): void {
+        if (this.size == this.capacity){
+            this.resize()
+        }
+        this.arr[this.size] = n
+        this.size += 1
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback(): number {
+        this.size -= 1
+        return this.arr[this.size]
+
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize(): void {
+         this.capacity *= 2
+         let newarr: number[] = new Array(this.capacity)
+        
+        for (let i = 0; i < this.size; i++) {
+        newarr[i] = this.arr[i];
+    }
+         this.arr = newarr
+
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize(): number {
+        return this.size
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity(): number {
+        return this.capacity
+    }
+}
