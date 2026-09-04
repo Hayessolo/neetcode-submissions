98f6a1a11be3400c885ca76a656b7ee8bb1c598b
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-09-04 12:55:52 +0300

    Add: number-of-students-unable-to-eat-lunch - submission-5

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Map<Integer,Integer> counter = new HashMap<>();
+        
+        for( int student: students){
+            counter.merge(student,1,Integer::sum);
+        }
+        for(var sandwich: sandwiches){
+            if(counter.getOrDefault(sandwich,0) > 0){
+                counter.merge(sandwich,-1,Integer::sum);
+            }else{
+                break;
+            }
+        }
+
+        return counter.values().stream().mapToInt(Integer::intValue).sum();
+
+    }
+}
