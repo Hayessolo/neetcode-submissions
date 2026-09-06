aabd1c02a4b730586b9829e30ad24e083c60aa56
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-09-06 23:07:44 +0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> store =Map.of( ')','(','}','{',']','[');
+        Stack<Character> counter = new Stack<>();
+        for(char  c : s.toCharArray() ){
+            if( store.containsKey(c) && !(counter.empty())){
+                if(store.get(c) == counter.peek()){
+                    counter.pop();
+                }else{
+                    return false;
+                }
+
+            }else{
+                counter.push(c);
+            }
+        }
+        if(counter.empty()){
+            return true;
+
+        }else{
+            return false ;
+        }
+        
+
+    }
+}
