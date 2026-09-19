4a1c1cf8f80c6b27327110053249086db477b704
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-09-19 12:21:16 +0300

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <=2 ){
+            return n;
+        }else{
+            
+            int[] steps = new int[n+1];
+            steps[1] = 1;
+            steps[2] = 2;
+            for(int i =3;i <= n;i++){
+                steps[i] = steps[i-1] + steps[i-2];
+
+            }
+            return steps[n];
+        }
+
+    }
+}
