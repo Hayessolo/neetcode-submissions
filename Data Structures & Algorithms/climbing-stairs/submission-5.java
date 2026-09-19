0f401cc24365ff345486e51da19436859e0cae1f
Hayes Frank <88054399+Hayessolo@users.noreply.github.com>
2026-09-19 11:08:47 +0300

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        Map<Integer,Integer> cach = new HashMap<>();
+        return climb(n,cach);
+            }
+    public  int climb(int n ,Map<Integer,Integer> cache){
+            if(n <= 2){
+                return n;
+            }
+            int val  = climb(n-1,cache) + climb(n-2,cache);
+            cache.put(n, val);
+            return cache.get(n);
+            
+        }
+
+
+
+}
